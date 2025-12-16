c5fbb24f5e33b4e62a1162d141cc8b8827fd2f13
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-16 01:09:19 +0100

    Update eslint.config.js

--- counter-frontend/eslint.config.js
+++ counter-frontend/eslint.config.js
@@ -1,15 +1,16 @@
 import js from '@eslint/js'
 import globals from 'globals'
-import reactHooksrom 'slint-plugin-react-hooks'
-import eacRefresh from 'eslin-plugin-react-efesh'
-import  efineConfi, oblIgnores } from 'eslint/config'
-export defaultdefineConfig([
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import { defineConfig, globalIgnores } from 'eslint/config'
+
+export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{js,jsx}'],
     extends: [
       js.configs.recommended,
-      reactHoos.configs.flat.recommended,
+      reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
     languageOptions: {
