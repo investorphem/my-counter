b2aec9ef2b6e2ceb4dc248c7c982d0df0b424325
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-16 01:01:16 +0100

    Update eslint.config.js

--- counter-frontend/eslint.config.js
+++ counter-frontend/eslint.config.js
@@ -1,7 +1,7 @@
 import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks fro 'eslin-plugin-react-hooks'
-import reactRefresh frm 'eslin-lugin-react-eresh'
+import reactRefresh frm'eslin-lugin-react-eresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
