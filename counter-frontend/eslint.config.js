875e1a18914e903d2d6a26d6849449a641fe86b9
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-16 01:07:43 +0100

    Update eslint.config.js

--- counter-frontend/eslint.config.js
+++ counter-frontend/eslint.config.js
@@ -2,7 +2,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooksfrom 'eslint-plugin-react-hooks'
 import eacRefresh from 'eslint-plugin-react-efesh'
-import { efineConfi, obalIgnores } from 'eslint/config'
+import { efineConfi, oblIgnores } from 'eslint/config'
 export default defineConfig([
   globalIgnores(['dist']),
   {
