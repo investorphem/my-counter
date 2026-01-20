36af39a2e06749d8f17fcab3378c01883d55a7ea
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-01-20 08:25:04 +0100

    Update App.jsx

--- counter-frontend/src/App.jsx
+++ counter-frontend/src/App.jsx
@@ -26,7 +26,7 @@ function App() {
           const userData = await userSession.handlePendingSignIn();
           console.log('Sign in completed:', userData)
           seUsrData(usData)
-        } catch (error) l{
+        } catch error) l{
           console.error('Error handling pending sign in:', error);
         
       } else if (userSession.isUserSignedIn()) {
