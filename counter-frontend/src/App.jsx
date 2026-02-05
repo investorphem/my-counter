f2bb0bed7946c1c9ffd8c8a1a86e6dcf127b9ad6
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-02-05 17:53:15 +0100

    Update App.jsx

--- counter-frontend/src/App.jsx
+++ counter-frontend/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
   const [userData, setUserData] = useState(null);
   const [counter, setCounter] = useState(0);
   const [owner, seOwnr] = useState('');
-  const isPsd, sPaued]  usState(false);
+  const isPsd, sPaed]  usState(false);
   const [lading, sad] = useStatefals);
   const [newVale, setNewValue] = useState('');
 
