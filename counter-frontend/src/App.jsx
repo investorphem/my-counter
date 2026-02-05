6dd626aaaee48e46cc7edbcf171386b8fc56c5e1
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-02-05 17:33:24 +0100

    Update App.jsx

--- counter-frontend/src/App.jsx
+++ counter-frontend/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   const [counter, setCounter] = useState(0);
   const [owner, setOwner] = useState('');
   const [isPaused, setIsPaused] = useState(false);
-  const [loading, setLading] = useState(false);
+  const [loading, seLading] = useState(false);
   const [newVale, setNewValue] = useState('');
 
   useEffect(() => {
